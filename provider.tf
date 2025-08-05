a6e4a38f3df4dc60f31ca4703552c60fd26fac83
Igor Rubio <ighor_souza@hotmailcom>
2025-08-05 17:52:04 -0300

    fix: backend

--- provider.tf
+++ provider.tf
@@ -7,20 +7,27 @@ terraform {
 
 
 
-     kubernetes = {
-       source  = "hashicorp/kubernetes"
-       version = "2.38.0"
-     }
 
-     helm = {
-       source  = "hashicorp/helm"
-       version = "3.0.2"
-     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.38.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
 
 
   }
 
 
+  backend "s3" {
+    bucket = "bucket-s3-state"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+
+  }
 
 }
 
@@ -29,25 +36,25 @@ provider "aws" {
 }
 
 
- provider "kubernetes" {
-   host                   = module.eks_cluster.endpoint
-   cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
-   exec {
-     api_version = "client.authentication.k8s.io/v1beta1"
-     args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
-     command     = "aws"
-   }
- }
+provider "kubernetes" {
+  host                   = module.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
+}
 
 
- provider "helm" {
-   kubernetes = {
-     host                   = module.eks_cluster.endpoint
-     cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
-     exec = {
-       api_version = "client.authentication.k8s.io/v1beta1"
-       args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
-       command     = "aws"
-     }
-   }
- }
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+      command     = "aws"
+    }
+  }
+}
